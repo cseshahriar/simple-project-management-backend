84dc44fd04d6404c87d5292dba65ca5362f6874a
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-08 03:48:01 +0600

    project model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=False)
+    category = models.CharField(max_length=255, null=True, blank=False)
+    description = models.TextField()
+    demo = models.CharField(max_length=255, null=True, blank=True)
+    thumbnail = models.ImageField(
+        upload_to="thumbnail/images", null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.title
--- server/urls.py
+++ server/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+# serve media files in development environment --------------------------------
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+admin.sites.AdminSite.site_header = "App Administration"
+admin.sites.AdminSite.site_title = "App Administration"
+admin.sites.AdminSite.index_title = "App Admin Panel"
